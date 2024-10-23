057fde71f9fbc02aef91e6197f0d6282639fec18
serleo <serleo96.98@gmail.com>
2024-10-23 20:07:21 -0300

    first commit

---
+++ src/repository/db/databaseConecction.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const { Client } = require('pg')
+const client = new Client({
+    user: process.env.Client,
+    host: 'SG-PostgreNoSSL-14-pgsql-master.devservers.scalegrid.io',
+    database: 'postgres',
+    password: 'password',
+    port: 5432,
+})
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
